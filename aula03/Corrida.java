b4cf80c9093c634c5c898036d1ea72df7e007e9a
Raul Ryan <ryanraul_11@hotmail.com>
2020-09-09 21:52:06 -0300

    Corrigindo Atividade da Corrida

--- aula03/Corrida.java
+++ aula03/Corrida.java
@@ -6,21 +6,20 @@ public class Corrida {
 
         double distanciaPistaMetros = 10000;
 
-        while(c1.distanciaPercorrida_m <= distanciaPistaMetros || c2.distanciaPercorrida_m <= distanciaPistaMetros){
+        while(c1.distanciaPercorrida_m <= distanciaPistaMetros && c2.distanciaPercorrida_m <= distanciaPistaMetros){
             c1.velocidadeKm_h = Math.random() * 200;
             //System.out.println("VEL C1 = " + c1.velocidadeKm_h);
             c2.velocidadeKm_h = Math.random() * 200;
             //System.out.println("VEL C2 = " + c2.velocidadeKm_h);
-            c1.mover(1);
-            c2.mover(1);
+            c1.mover(10);
+            c2.mover(10);
         }
-
+        
         if(c1.distanciaPercorrida_m > c2.distanciaPercorrida_m){
             System.out.println("C1 VENCEU!");
         }else{
             System.out.println("C2 VENCEU!");
-        }
-
+        } 
 
     }
 }
